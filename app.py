79686613b2d829e678095ea705653f61563ff531
Piet Ysabie <piet.ysabie@yahoo.com>
2026-04-23 12:16:53 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 from flask import Flask, request, jsonify
-from yahoo_fin.stock_info import get_live_price
+from yahoo_fin.stock_info import get_data
+import traceback
 
 app = Flask(__name__)
 
 @app.route("/price")
 def price():
     ticker = request.args.get("ticker")
-    if not ticker:
-        return jsonify({"error": "No ticker provided"})
+    
     try:
-        price = get_live_price(ticker)
-        return jsonify({"price": price})
+        df = get_data(ticker, interval="1d", index_as_date=False)
+        price = df.iloc[-1]["close"]
+        return jsonify({"price": float(price)})
     except Exception as e:
-        return jsonify({"error": str(e)})
-
+        return jsonify({
+            "error": str(e),
+            "trace": traceback.format_exc()
+        })
 # nodig voor deployment
 if __name__ == "__main__":
     app.run()
+
+
+    
+
+
+
