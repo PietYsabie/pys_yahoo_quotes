d5eec56513246e94555167d8ca226d2cb2c65401
Piet Ysabie <piet.ysabie@yahoo.com>
2026-04-23 12:23:43 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
 from flask import Flask, request, jsonify
 from yahoo_fin.stock_info import get_data
 import traceback
+import requests
 
 app = Flask(__name__)
+
+
 
 @app.route("/price")
 def price():
     ticker = request.args.get("ticker")
     
-    try:
-        df = get_data(ticker, interval="1d", index_as_date=False)
-        price = df.iloc[-1]["close"]
-        return jsonify({"price": float(price)})
-    except Exception as e:
-        return jsonify({
-            "error": str(e),
-            "trace": traceback.format_exc()
-        })
-# nodig voor deployment
-if __name__ == "__main__":
-    app.run()
-
-
+    url = f"https://query1.finance.yahoo.com/v8/finance/chart/{ticker}"
     
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
 
+    res = requests.get(url, headers=headers)
+    
+    try:
+        data = res.json()
+        price = data["chart"]["result"][0]["meta"]["regularMarketPrice"]
+        return jsonify({"price": price})
+    except:
+        return jsonify({
+            "error": "Yahoo blocked or invalid response",
+            "raw": res.text[:200]
+        })
 
 
